85f3dc0bdecfbf677b4cf87caf91b280e3e2207e
Ben <ben.larson@hotmail.com>
2024-08-21 07:27:02 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # sns-notifications-lib
+Library for interacting with my [central notifications service](https://github.com/BenD0G/playground/tree/master/notifications).
---
+++ src/lib.rs
@@ -0,0 +1,43 @@
+use anyhow::Result;
+use serde::{Deserialize, Serialize};
+
+/// The payload to be sent to the central notification lambda.
+#[derive(Serialize, Deserialize)]
+pub struct NotificationPayload {
+    /// The API key to retrieve from Parameter Store, which determines which Pushover channel to use.
+    pub api_key_name: String,
+    /// The title of the notification, to appear in the push notification.
+    pub title: String,
+    /// The message of the notification, to appear in the push notification.
+    pub message: String,
+}
+
+pub async fn load_sns_client_from_env() -> aws_sdk_sns::Client {
+    let config = aws_config::load_from_env().await;
+    aws_sdk_sns::Client::new(&config)
+}
+
+/// Send a message to the pushover-notifications SNS topic, which will get picked up by the central notification lambda
+/// and routed to the appropriate Pushover channel.
+pub async fn publish_sns_message(
+    client: &aws_sdk_sns::Client,
+    payload: &NotificationPayload,
+) -> Result<()> {
+    let message = serde_json::to_string(payload)?;
+
+    client
+        .publish()
+        .topic_arn("arn:aws:sns:eu-west-2:982932998640:pushover-notifications")
+        .message(&message)
+        .send()
+        .await?;
+
+    Ok(())
+}
+
+/// Loads a SNS client and sends a message to the pushover-notifications SNS topic, which will get picked up
+/// by the central notification lambda and routed to the appropriate Pushover channel.
+pub async fn load_client_and_send_notification(payload: &NotificationPayload) -> Result<()> {
+    let client = load_sns_client_from_env().await;
+    publish_sns_message(&client, &payload).await
+}
